3b7091e749d754c7c7445cb0114c0618f5d68e49
Mohammad Thareeq <mohammadthareeq@gmail.com>
2020-01-24 10:59:21 +0700

    p1

---
+++ p1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a;
+    // cout << "masukkan angka: ";
+    // cin >> a;
+    // cout << "tes " << a << endl;
+    return 0;
+}
